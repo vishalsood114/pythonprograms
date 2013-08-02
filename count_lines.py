e3507f9863f7f33415729155c275f0a296139575
Vishal Sood <vishalsood114@gmail.com>
2013-08-02 14:38:18 +0530

    counts all the lines for mulitple files passed as argument

---
+++ count_lines.py
@@ -0,0 +1,26 @@
+import os
+#import grep
+import sys
+
+##def count_lines(f):
+##    return len(open(f).readlines())
+
+def read_files (filenames):
+    for f in filenames: 
+        for line in open(f):
+            yield line
+
+def count_iter(seq):
+    count = 0
+    for x in seq:
+        count += 1
+    return count
+
+def main():
+    filenames = sys.argv[1:]
+    lines = read_files(filenames)       
+    linecount = count_iter (lines)
+    print linecount
+
+if __name__ == "__main__":
+    main()
