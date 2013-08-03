a64596a160c36c16a077b51c55fc6f661a96908b
Vishal Sood <vishalsood114@gmail.com>
2013-08-03 16:40:09 +0530

    adding our own custom not found message

--- webappl.py
+++ webappl.py
@@ -1,6 +1,6 @@
 """The web application calling the web framework bicycle"""
 
-from bicycle import request, before_request, route
+from bicycle import request, before_request, route, on_notfound
 import time
 
 @before_request
@@ -8,8 +8,12 @@ def log_request(url):
     print time.asctime(), url
 
 @route("/")
-def index:
+def index():
     return "Welcome"
+
+@on_notfound
+def not_found():
+    return "404 - This is not the page your are looking for."
 
 def main():
     print request("/")
