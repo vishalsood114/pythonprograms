b13334537718500133c7609fffbe0df1ff6b9edb
Vishal Sood <vishalsood114@gmail.com>
2013-08-03 13:11:16 +0530

    program calculates time taken to run a method using wrappers (or decorators)

---
+++ profile.py
@@ -0,0 +1,21 @@
+import time
+
+def profile(f):
+    def g(n):
+        t0 = time.time()
+        value = f(n)
+        t1 = time.time()
+        print "took %f seconds" % (t1-t0)
+        return value
+    return g
+
+def timepass(n):
+    sum = 0
+    for i in range(n):
+        for j in range(n):
+            sum += i*j
+    return sum
+
+timepass = profile(timepass)
+
+print timepass(10)
