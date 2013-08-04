cbdcbc8d2063ad70bb7ac90ab7eb7e3d87e3733f
Vishal Sood <vishalsood114@gmail.com>
2013-08-04 09:20:47 +0530

    modifying the webframework to register the route

--- bicycle.py
+++ bicycle.py
@@ -1,6 +1,7 @@
 """A simple webframework """
 
 before_hooks = []
+mapping = []
 
 def default_notfound():
     return "Not Found"
@@ -10,6 +11,11 @@ notfound = default_notfound
 def request(url):
     for h in before_hooks:
         h(url)
+
+    for pattern, f in mapping:
+        if pattern == url:
+            return f()
+
     return notfound()
 
 def before_request(f):
@@ -17,10 +23,12 @@ def before_request(f):
     return f
 
 def route(path):
-    def fake_decor(f):
+    #print "route", path, mapping
+    def decorator(f):
+        mapping.append((path,f))
+        #print "decorator" , f, mapping
         return f
-
-    return fake_decor
+    return decorator
 
 def on_notfound(f):
     global notfound
